251f99aec7efa0d2adabdb09237461728f5c72bd
umerkhan24 <124992555+umerkhan24@users.noreply.github.com>
2023-02-10 19:07:27 +0500

    Add files via upload

---
+++ pythoncodes.py
@@ -0,0 +1,56 @@
+#Variable code
+x = 5
+y = "John"
+print(x)
+print(y)
+
+#String code
+a = "Hello"
+print(a)
+
+#list code
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+#tuple code
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+#set code
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+
+#dictionaries code
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
+#If-else code
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+#while loop code
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+
+  #for loop code
+  fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+#function code
+def my_function():
+  print("Hello from a function")
+
+my_function()
